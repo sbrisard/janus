55791b4341e451eeae3b194605ce53580430736f
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2013-09-16 17:06:56 +0200

    Implementation of Green operator in plane strains.

--- greenop.pyx
+++ greenop.pyx
@@ -3,29 +3,72 @@ from libc.math cimport sqrt
 
 from matprop cimport IsotropicLinearElasticMaterial as Material
 
+cdef double SQRT_TWO = sqrt(2.)
+
 cdef class GreenOperator2d:
     cdef readonly int dim
     cdef int sym
     cdef readonly Material mat
 
+    cdef double daux1, daux2, daux3, daux4
+
+    
     def __cinit__(self, Material mat):
         self.dim = 2
         self.sym = 3
         if (self.dim != mat.dim):
             raise ValueError('plane strain material expected')
         self.mat = mat
+        cdef double mu = mat.g
+        cdef double nu = mat.nu
+        self.daux1 = 1.0 / mu
+        self.daux2 = 0.5 / (mu * (1.0 - nu))
+        self.daux3 = 0.25 / mu
+        self.daux4 = 0.5 / mu
+        
         
     @boundscheck(False)
-    cpdef apply(self, double[::1] k, double[::1] tau):
+    cpdef double[::1] apply(self,
+                            double[::1] k,
+                            double[::1] tau,
+                            double[::1] eps):
+        
         # The following tests are necessary, since bounds checks are removed.
         if k.shape[0] != self.dim:
             raise IndexError('shape of k must be ({0},)'.format(self.dim))
 
         if tau.shape[0] != self.sym:
             raise IndexError('shape of tau must be ({0},)'.format(self.sym))
+
+        if eps.shape[0] != self.sym:
+            raise IndexError('shape of eps must be ({0},)'.format(self.sym))
         
-        cdef double nx = k[0]
-        cdef double ny = k[1]
-        cdef double scale = 1. / sqrt(nx * nx + ny * ny)
-        nx *= scale
-        ny *= scale
+        cdef double kx = k[0]
+        cdef double ky = k[1]
+        cdef double kxkx = kx * kx
+        cdef double kyky = ky * ky
+        if kxkx + kyky == 0.:
+            eps[0] = 0.
+            eps[1] = 0.
+            eps[2] = 0.
+            return eps
+
+        cdef double s = 1.0 / (kxkx + kyky)
+        kxkx *= s
+        kyky *= s
+        cdef double kxky = s * kx * ky
+        
+        cdef double m00 = kxkx * (self.daux1 - self.daux2 * kxkx)
+        cdef double m11 = kyky * (self.daux1 - self.daux2 * kyky)
+
+        cdef double dummy = -self.daux2 * kxkx * kyky
+        cdef double m01 = dummy
+        cdef double m22 = 2 * (self.daux3 + dummy)
+        cdef double m02 = SQRT_TWO * kxky * (self.daux4 - self.daux2 * kxkx)
+        cdef double m12 = SQRT_TWO * kxky * (self.daux4 - self.daux2 * kyky)
+
+        eps[0] = m00 * tau[0] + m01 * tau[1] + m02 * tau[2]
+        eps[1] = m01 * tau[0] + m11 * tau[1] + m12 * tau[2]
+        eps[2] = m02 * tau[0] + m12 * tau[1] + m22 * tau[2]
+        
+        return eps
--- greenoptest.py
+++ greenoptest.py
@@ -1,5 +1,13 @@
+import numpy as np
+from numpy import cos
+from numpy import sin
+
+from nose.tools import assert_almost_equal
+from nose.tools import nottest
+
 import mandelvoigt
 from matprop import IsotropicLinearElasticMaterial as Material
+from greenop import GreenOperator2d
 
 def delta(i, j):
     if i == j:
@@ -9,17 +17,61 @@ def delta(i, j):
 
 def green_coefficient(i, j, k, l, n, mat):
     return ((delta(i, k) * n[j] * n[l]
-             + delta(i, l) * n[i] * n[k]
+             + delta(i, l) * n[j] * n[k]
              + delta(j, k) * n[i] * n[l]
-             + delta(j, l) * n[i] * n[k]) / mat.g
+             + delta(j, l) * n[i] * n[k]) / (4. * mat.g)
             - n[i] * n[j] * n[k] * n[l] / (2. * mat.g * (1. - mat.nu)))
 
 def green_matrix(n, mat):
-    return mandelvoigt.get_instance(mat.dim).create_array(green_coefficient, (n, mat))
+    return mandelvoigt.get_instance(mat.dim).create_array(green_coefficient, n, mat)
+
+@nottest
+def do_test_apply(k, mat):
+    n = k / np.sqrt(sum(k**2))
+    expected = green_matrix(n,  mat)
+    sym = (mat.dim * (mat.dim + 1)) // 2
+    green = GreenOperator2d(mat)
+    actual = np.empty((sym,), np.float64)
+
+    for j in range(sym):
+        tau = np.zeros((sym,), np.float64)
+        tau[j] = 1.
+        green.apply(k, tau, actual)
+        for i in range(sym):
+            msg = 'coefficient [{0}, {1}]'.format(i, j)
+            assert_almost_equal(expected[i, j], actual[i],
+                                msg = msg, delta = 1.E-15)
+
+def test_apply_2d():
+    mat = Material(0.7, 0.3, 2)
+    k_norms = [2.5, 3.5]
+    num_angles = 20
+    k_angles = [(2. * np.pi * i) / num_angles for i in range(num_angles)]
+
+    vec = lambda r, theta: np.array([r * cos(theta), r * sin(theta)],
+                                    dtype=np.float64)
+    
+    for k in k_norms:
+        for theta in k_angles:
+            yield do_test_apply, vec(k, theta), mat
 
 if __name__ == '__main__':
-    import numpy as np
+    test_apply_2d_1()
 
-    mat = Material(g=1., nu=0.3)
-    n = np.array([0., 0., 1.])
-    a = green_matrix(n, mat)
+def test_apply_3d():
+    mat = Material(0.7, 0.3, 3)
+    norms = [2.5, 3.5]
+    num_thetas = 10
+    num_phis = 20
+    thetas = [(np.pi * i) / num_thetas for i in range(num_thetas)]
+    phis = [(2. * np.pi * i) / num_phis for i in range(num_phis)]
+
+    vec = lambda r, theta, phi: np.array([r * cos(theta) * cos(phi),
+                                          r * cos(theta) * sin(phi),
+                                          r * sin(theta)],
+                                         dtype=np.float64)
+    
+    for k in norms:
+        for theta in thetas:
+            for phi in phis:
+                yield do_test_apply, vec(k, theta, phi), mat
