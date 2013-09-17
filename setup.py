ee65896387b8761a21bcc8e9efc2c4e89795a95e
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2013-09-17 11:54:20 +0200

    Cosmetic changes.

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ import numpy
 
 include_dirs = [numpy.get_include()]
 
-ext_modules = [Extension('matprop', ['matprop.pyx'], include_dirs=include_dirs),
-               Extension('greenop', ['greenop.pyx'], include_dirs=include_dirs)]
+ext_modules = [Extension('matprop', ['matprop.pyx'],
+                         include_dirs=include_dirs),
+               Extension('greenop', ['greenop.pyx'],
+                         include_dirs=include_dirs)]
 
-setup(
-  name = 'Homogenization through FFT',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
-)
+setup(name = 'Homogenization through FFT',
+      cmdclass = {'build_ext': build_ext},
+      ext_modules = ext_modules)
