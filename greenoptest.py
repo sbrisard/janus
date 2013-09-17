2c055e8e98c663c8aabafcd88c8f8db5d9a9c697
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2013-09-17 17:47:36 +0200

    Refactored unit tests for GreenOperatornd.apply(), and added tests for GreenOperatornd.asmatrix().

--- greenoptest.py
+++ greenoptest.py
@@ -6,10 +6,14 @@ from nose.tools import assert_almost_equal
 from nose.tools import assert_equal
 from nose.tools import nottest
 
-import mandelvoigt
+import mandelvoigt as mv
 from matprop import IsotropicLinearElasticMaterial as Material
 from greenop import GreenOperator2d
 from greenop import GreenOperator3d
+
+DIMS = [2, 3]
+MU = 0.7
+NU = 0.3
 
 def delta(i, j):
     if i == j:
@@ -24,20 +28,51 @@ def green_coefficient(i, j, k, l, n, mat):
              + delta(j, l) * n[i] * n[k]) / (4. * mat.g)
             - n[i] * n[j] * n[k] * n[l] / (2. * mat.g * (1. - mat.nu)))
 
-def green_matrix(n, mat):
-    return mandelvoigt.get_instance(mat.dim).create_array(green_coefficient, n, mat)
+def green_matrix(k, mat):
+    k2 = sum(k**2)
+    if k2 == 0.:
+        sym = (mat.dim * (mat.dim + 1)) // 2
+        return np.zeros((sym, sym), dtype=np.float64)
+    else:
+        n = k / np.sqrt(k2)
+        return mv.get_instance(mat.dim).create_array(green_coefficient, n, mat)
+
+def create_green_operator(mat):
+    if mat.dim == 2:
+        return GreenOperator2d(mat)
+    elif mat.dim == 3:
+        return GreenOperator3d(mat)
+
+def wave_vectors(dim):
+    norms = [2.5, 3.5]
+    if dim == 2:
+        num_angles = 20
+        thetas = [(2. * np.pi * i) / num_angles for i in range(num_angles)]
+        def vec(r, theta):
+            return np.array([r * cos(theta), r * sin(theta)], dtype=np.float64)
+        ret = [vec(k, theta) for theta in thetas for k in norms]
+    elif dim == 3:
+        num_thetas = 10
+        num_phis = 20
+        thetas = [(np.pi * i) / num_thetas for i in range(num_thetas)]
+        phis = [(2. * np.pi * i) / num_phis for i in range(num_phis)]
+        def vec(r, theta, phi):
+            return np.array([r * cos(theta) * cos(phi),
+                             r * cos(theta) * sin(phi),
+                             r * sin(theta)],
+                            dtype=np.float64)
+        ret = [vec(k, theta, phi) for phi in phis for theta in thetas
+               for k in norms]
+    else:
+        raise ValueError()
+    ret.append(np.zeros((dim,), dtype=np.float64))
+    return ret
 
 @nottest
 def do_test_apply(k, mat):
-    n = k / np.sqrt(sum(k**2))
-    expected = green_matrix(n,  mat)
+    expected = green_matrix(k,  mat)
     sym = (mat.dim * (mat.dim + 1)) // 2
-    if mat.dim == 2:
-        green = GreenOperator2d(mat)
-    elif mat.dim == 3:
-        green = GreenOperator3d(mat)
-    else:
-        raise ValueError()
+    green = create_green_operator(mat)
     actual = np.empty((sym,), np.float64)
 
     for j in range(sym):
@@ -47,57 +82,29 @@ def do_test_apply(k, mat):
         for i in range(sym):
             msg = 'coefficient [{0}, {1}]'.format(i, j)
             assert_almost_equal(expected[i, j], actual[i],
-                                msg = msg, delta = 1.E-15)
+                                msg=msg, delta=1.E-15)
 
-def test_apply_2d():
-    mat = Material(0.7, 0.3, 2)
-    k_norms = [2.5, 3.5]
-    num_angles = 20
-    k_angles = [(2. * np.pi * i) / num_angles for i in range(num_angles)]
-
-    vec = lambda r, theta: np.array([r * cos(theta), r * sin(theta)],
-                                    dtype=np.float64)
-    
-    for k in k_norms:
-        for theta in k_angles:
-            yield do_test_apply, vec(k, theta), mat
-
-def test_apply_3d():
-    mat = Material(0.7, 0.3, 3)
-    norms = [2.5, 3.5]
-    num_thetas = 10
-    num_phis = 20
-    thetas = [(np.pi * i) / num_thetas for i in range(num_thetas)]
-    phis = [(2. * np.pi * i) / num_phis for i in range(num_phis)]
-
-    vec = lambda r, theta, phi: np.array([r * cos(theta) * cos(phi),
-                                          r * cos(theta) * sin(phi),
-                                          r * sin(theta)],
-                                         dtype=np.float64)
-    
-    for k in norms:
-        for theta in thetas:
-            for phi in phis:
-                yield do_test_apply, vec(k, theta, phi), mat
+def test_apply():
+    for dim in DIMS:
+        mat = Material(MU, NU, dim)
+        for k in wave_vectors(dim):
+            yield do_test_apply, k, mat
 
 @nottest
-def do_test_apply_null_wave_vector(dim):
-    mat = Material(0.7, 0.3, dim)
-    if dim == 2:
-        green = GreenOperator2d(mat)
-    elif dim == 3:
-        green = GreenOperator3d(mat)
-    k = np.zeros((dim,), dtype=np.float)
-    sym = (dim * (dim + 1)) // 2
-    for i in range(sym):
-        tau = np.zeros((sym,), dtype=np.float)
-        tau[i] = 1.
-        eps = green.apply(k, tau)
-        for j in range(sym):
+def do_test_asmatrix(k, mat):
+    expected = green_matrix(k,  mat)
+    sym = (mat.dim * (mat.dim + 1)) // 2
+    green = create_green_operator(mat)
+    actual = np.empty((sym, sym), np.float64)
+    green.asmatrix(k, actual)
+    for j in range(sym):
+        for i in range(sym):
             msg = 'coefficient [{0}, {1}]'.format(i, j)
-            assert_equal(eps[j], 0., msg=msg)
+            assert_almost_equal(expected[i, j], actual[i, j],
+                                msg=msg, delta=1.E-15)
 
-def test_apply_null_wave_vector():
-    dims = [2, 3]
-    for dim in dims:
-        yield do_test_apply_null_wave_vector, dim
+def test_asmatrix():
+    for dim in DIMS:
+        mat = Material(MU, NU, dim)
+        for k in wave_vectors(dim):
+            yield do_test_asmatrix, k, mat
