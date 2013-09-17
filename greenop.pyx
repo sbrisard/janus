963fd6fe335aeb4fa90608f8d8e15e34499a0a43
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2013-09-17 10:08:37 +0200

    In class GreenOperator2d, added docstrings. Moreover, allow for eps=None in GreenOperator2d.apply(k, tau, eps=None).

--- greenop.pyx
+++ greenop.pyx
@@ -1,3 +1,6 @@
+import numpy as np
+cimport numpy as np
+
 from cython cimport boundscheck
 from libc.math cimport sqrt
 
@@ -8,33 +11,76 @@ cdef double SQRT_TWO = sqrt(2.)
 cdef class GreenOperator2d:
 
     """This class defines the periodic Green operator for 2d (plane
-    strain elasticity.
+    strain) elasticity.
+
+    Parameters
+    ----------
+    mat : IsotropicLinearElasticMaterial
+        Reference material.
+
+    Attributes
+    ----------
+    dim : int
+        Dimension of the physical space.
+    mat : IsotropicLinearElasticMaterial
+        Reference material.
+    sym : int
+        Dimension of the space on which this object operates (space of
+        the second-rank, symmetric tensors).
+    daux1 : double
+        Value of `1 / g`, where `g` is the shear modulus of the reference
+        material.
+    daux2 : double
+        Value of `1 / 2 / g / (1 - nu)`, where `g` (resp. `nu`) is the
+        shear modulus (resp. Poisson ratio) of the reference material.
+    daux3 : double
+        Value of `1 / 4 / g`, where `g` is the shear modulus of the
+        reference material.
+    daux4 : double
+        Value of `1 / g`, where `g` is the shear modulus of the
+        reference material.
 
     """
 
     cdef:
-        readonly int dim, sym
+        readonly int dim
         readonly Material mat
+        int sym
         double daux1, daux2, daux3, daux4
 
-    cdef double daux1, daux2, daux3, daux4
-
-    
     def __cinit__(self, Material mat):
         self.dim = 2
         self.sym = 3
         if (self.dim != mat.dim):
             raise ValueError('plane strain material expected')
         self.mat = mat
-        cdef double mu = mat.g
+        cdef double g = mat.g
         cdef double nu = mat.nu
-        self.daux1 = 1.0 / mu
-        self.daux2 = 0.5 / (mu * (1.0 - nu))
-        self.daux3 = 0.25 / mu
-        self.daux4 = 0.5 / mu
+        self.daux1 = 1.0 / g
+        self.daux2 = 0.5 / (g * (1.0 - nu))
+        self.daux3 = 0.25 / g
+        self.daux4 = 0.5 / g
 
     @boundscheck(False)
-    cpdef double[:] apply(self, double[:] k, double[:] tau, double[:] eps):
+    cpdef double[:] apply(self, double[:] k, double[:] tau,
+                          double[:] eps = None):
+        """ Apply the Green operator to the specified prestress.
+
+        Parameters
+        ----------
+        k : array_like
+            The wave-vector.
+        tau : array_like
+            The value of the prestress for the specified mode `k`.
+        eps : array_like, optional
+            The result of the operation `Gamma(k) : tau`. Strictly
+            speaking, `eps` is the opposite of a strain.
+
+        Returns
+        -------
+        eps : array_like
+            The result of the linear operation `Gamma(k) : tau`.
+        """
 
         # The following tests are necessary, since bounds checks are removed.
         if k.shape[0] != self.dim:
@@ -43,8 +89,11 @@ cdef class GreenOperator2d:
         if tau.shape[0] != self.sym:
             raise IndexError('shape of tau must be ({0},)'.format(self.sym))
 
-        if eps.shape[0] != self.sym:
-            raise IndexError('shape of eps must be ({0},)'.format(self.sym))
+        if eps is not None:
+            if eps.shape[0] != self.sym:
+                raise IndexError('shape of eps must be ({0},)'.format(self.sym))
+        else:
+            eps = np.empty((self.sym,), dtype = np.float64)
 
         cdef double kx = k[0]
         cdef double ky = k[1]
--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
-ext_modules = [Extension('matprop', ['matprop.pyx']),
-               Extension('greenop', ['greenop.pyx'])]
+include_dirs = [numpy.get_include()]
+
+ext_modules = [Extension('matprop', ['matprop.pyx'], include_dirs=include_dirs),
+               Extension('greenop', ['greenop.pyx'], include_dirs=include_dirs)]
 
 setup(
   name = 'Homogenization through FFT',
