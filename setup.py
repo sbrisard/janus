404629d3844c1a9143124e12c23feb0a1c2b7c5e
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2013-09-24 14:06:37 +0200

    In setup.py, listed *.pxd files.

--- setup.py
+++ setup.py
@@ -5,11 +5,16 @@ import numpy
 
 include_dirs = [numpy.get_include()]
 
-ext_modules = [Extension('matprop', ['matprop.pyx'],
+ext_modules = [Extension('matprop',
+                         sources=['matprop.pxd', 'matprop.pyx'],
                          include_dirs=include_dirs),
-               Extension('greenop', ['greenop.pyx'],
+               Extension('greenop',
+                         sources=['greenop.pxd', 'greenop.pyx'],
+                         include_dirs=include_dirs),
+               Extension('discretegreenop',
+                         sources=['discretegreenop.pyx'],
                          include_dirs=include_dirs)]
-
+    
 setup(name = 'Homogenization through FFT',
       cmdclass = {'build_ext': build_ext},
       ext_modules = ext_modules)
