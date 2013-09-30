dcc005564ded801d2f197fc1b5022973ef7701e5
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2013-09-30 15:36:29 +0200

    In SerialFFT2D   - use memoryviews instead of ndarrays   - optimized loops through memoryviews   - in_shape and out_shape are now accessible from within python.

--- serialfft.pyx
+++ serialfft.pyx
@@ -1,8 +1,11 @@
 from fftw cimport *
 
-import numpy as np
-cimport numpy as np
-np.import_array()
+cimport cython
+from cython.view cimport array
+
+cdef int SIZEOF_DOUBLE = sizeof(double)
+cdef int SIZEOF_COMPLEX = 2 * sizeof(double)
+cdef str INVALID_SHAPE_LENGTH = 'length of shape must be {0} (was {1})'
 
 cdef int padding(int n):
     if n % 2 == 0:
@@ -14,22 +17,26 @@ cdef class SerialFFT2D:
     cdef:
         double *buffer
         fftw_plan plan_r2c
-        # TODO specifiy type of elements in tuple
-        # TODO these attributes are not visible in Python.
-        tuple ishape, oshape
+        readonly tuple in_shape, out_shape
+        int dim
         int padding
+        int isize0, isize1, osize0, osize1
 
-    def __cinit__(self, tuple shape):
-        # TODO check that shape is a pair
-        cdef int n0 = shape[0]
-        cdef int n1 = shape[1]
-        self.ishape = shape
-        self.oshape = n0, n1 / 2 + 1
-        self.padding = padding(n1)
+    @cython.boundscheck(False)
+    def __cinit__(self, tuple size not None):
+        self.dim = 2
+        if len(size) != 2:
+            raise ValueError(INVALID_SHAPE_LENGTH.format(self.dim, len(size)))
+        self.isize0 = size[0]
+        self.isize1 = size[1]
+        self.in_shape = self.isize0, self.isize1
+        self.osize0 = self.isize0
+        self.osize1 = 2 * (self.isize1 / 2 + 1)
+        self.out_shape = self.osize0, self.osize1
+        self.padding = padding(self.isize1)
 
-        cdef size_t n = 2 * n0 * (n1 / 2 + 1)
-        self.buffer = fftw_alloc_real(n)
-        self.plan_r2c = fftw_plan_dft_r2c_2d(n0, n1,
+        self.buffer = fftw_alloc_real(self.osize0 * self.osize1)
+        self.plan_r2c = fftw_plan_dft_r2c_2d(self.isize0, self.isize1,
                                              <double *> self.buffer,
                                              <fftw_complex *> self.buffer,
                                              FFTW_ESTIMATE)
@@ -38,31 +45,47 @@ cdef class SerialFFT2D:
         fftw_free(self.buffer)
         fftw_destroy_plan(self.plan_r2c)
 
-    def r2c(self,
-            np.ndarray[np.float64_t, ndim=2] ain,
-            np.ndarray[np.complex128_t, ndim=2] aout = None):
-        cdef int i, j, offset
-        # TODO declare type of self.ishape so that loop gets optimized.
-        offset = 0
-        for i in range(self.ishape[0]):
-            for j in range(self.ishape[1]):
-                self.buffer[offset] = ain[i, j]
-                offset += 1
-            offset += self.padding
+    @cython.boundscheck(False)
+    @cython.cdivision(True)
+    @cython.wraparound(False)
+    cpdef double[:, :] r2c(self, double[:, :] ain, double[:, :] aout = None):
+        cdef:
+            int i0, i1, s0, s1
+            double *pbuf, *row, *cell
+
+        # Copy ain to self.buffer, using C pointers increments.
+        # Strides are expressed in terms of chars, not doubles.
+        s0 = ain.strides[0] / SIZEOF_DOUBLE
+        s1 = ain.strides[1] / SIZEOF_DOUBLE
+        pbuf = self.buffer
+        row = &ain[0, 0]
+        for i0 in range(self.isize0):
+            cell = row 
+            for i1 in range(self.isize1):
+                pbuf[0] = cell[0]
+                pbuf += 1
+                cell += s1
+            row += s0
+            pbuf += self.padding
         
         fftw_execute(self.plan_r2c)
         if aout is None:
-            aout = np.empty(self.oshape, np.complex128)
-        # TODO declare type of self.ishape so that loop gets optimized.
-        offset = 0
-        for i in range(self.oshape[0]):
-            for j in range(self.oshape[1]):
-                aout[i, j] = self.buffer[offset] + 1j * self.buffer[offset + 1]
-                offset += 2
+            aout = array(shape=self.out_shape,
+                         itemsize=SIZEOF_DOUBLE,
+                         format='d')
+        
+        # Copy result from self.buffer to aout
+        s0 = aout.strides[0] / SIZEOF_DOUBLE
+        s1 = aout.strides[1] / SIZEOF_DOUBLE
+        pbuf = self.buffer
+        row = &aout[0, 0]
+        for i0 in range(self.osize0):
+            cell = row
+            for i1 in range(self.osize1):
+                cell[0] = pbuf[0]
+                pbuf += 1
+                cell += s1
+            row += s0
+
         return aout
         
-    def in_shape(self):
-        return self.ishape
-                
-    def out_shape(self):
-        return self.oshape
--- serialffttest.py
+++ serialffttest.py
@@ -1,5 +1,6 @@
 from nose.tools import assert_less
 from nose.tools import nottest
+from nose.tools import raises
 import numpy as np
 import numpy.random as nprnd
 import numpy.fft as npfft
@@ -10,8 +11,9 @@ def do_test_r2c_2d(shape, delta):
 
     fft = SerialFFT2D(shape)
     a = 2. * nprnd.rand(*shape) - 1.
-    actual = np.empty(fft.out_shape(), dtype=np.complex128)
-    fft.r2c(a, actual)
+    dummy = np.empty(fft.out_shape, dtype=np.float64)
+    fft.r2c(a, dummy)
+    actual = dummy[:, 0::2] + 1j * dummy[:, 1::2]
     expected = npfft.rfft2(a)
 
     error = (np.sum(np.absolute(actual - expected))
@@ -27,3 +29,15 @@ def test_r2c_2d():
     for shape, delta in params:
         yield do_test_r2c_2d, shape, delta
 
+@nottest
+@raises(ValueError)
+def do_test_invalid_dimension(dim):
+    if dim == 2:
+        return SerialFFT2D((128, 128, 128))
+    else:
+        return SerialFFT3D((128, 128))
+
+def test_invalid_dimension():
+    dims = [2]
+    for dim in dims:
+        yield do_test_invalid_dimension, dim
