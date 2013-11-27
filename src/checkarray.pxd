d9366e5f96e6166fc851f1912575fa72dbd252d8
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2013-11-27 09:48:10 +0100

    In module checkarray, added methods to check the shape of a 3D array of doubles.

--- src/checkarray.pxd
+++ src/checkarray.pxd
@@ -14,3 +14,8 @@ cdef double[:] create_or_check_shape_1d(double[:] a, int n0) except *
 cdef void check_shape_2d(double[:, :] a, int n0, int n1) except *
 cdef double[:, :] create_or_check_shape_2d(double[:, :] a,
                                            int n0, int n1) except *
+
+cdef void check_shape_3d(double[:, :, :] a,
+                         int n0, int n1, int n2) except *
+cdef double[:, :, :] create_or_check_shape_3d(double[:, :, :] a,
+                                              int n0, int n1, int n2) except *
--- src/checkarray.pyx
+++ src/checkarray.pyx
@@ -23,6 +23,18 @@ cdef inline double[:, :] create_or_check_shape_2d(double[:, :] a,
     check_shape_2d(a, n0, n1)
     return a
 
+cdef inline void check_shape_3d(double[:, :, :] a,
+                                int n0, int n1, int n2) except *:
+    if (a.shape[0] != n0) or (a.shape[1] != n1) or (a.shape[2] != n2):
+        raise ValueError('invalid shape: expected ({0}, {1}, {2}), actual ({3}, {4}, {5})'.format(n0, n1, n2, a.shape[0], a.shape[1], a.shape[2]))
+
+cdef inline double[:, :, :] create_or_check_shape_3d(double[:, :, :] a,
+                                                     int n0, int n1, int n2) except *:
+    if a is None:
+        return array((n0, n1, n2), sizeof(double), 'd')
+    check_shape_3d(a, n0, n1, n2)
+    return a
+
 cdef inline void check_shape_ssize_t_1D(Py_ssize_t[:] a, int n0) except *:
     if a.shape[0] != n0:
         raise ValueError('invalid shape: expected ({0},), actual ({1},)'
