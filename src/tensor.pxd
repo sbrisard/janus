ed7d26fe5dcfcbd4dcb7120fa1ff5ff9d414af26
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2013-12-18 11:52:43 +0100

    In module tensor, removed classes   - IsotropicFourthRankTensor2D   - IsotropicFourthRankTensor3D.

--- src/tensor.pxd
+++ src/tensor.pxd
@@ -9,17 +9,3 @@ cdef class IsotropicFourthRankTensor:
     cdef isotropic4_apply_t static_c_apply
 
     cdef inline void c_apply(self, double[:] x, double[:] y)
-
-cdef class IsotropicFourthRankTensor2D:
-    cdef int dim
-    cdef readonly double sph, dev
-    cdef double s
-
-    cdef inline void c_apply(self, double[:] x, double[:] y)
-
-cdef class IsotropicFourthRankTensor3D:
-    cdef int dim
-    cdef readonly double sph, dev
-    cdef double s
-
-    cdef inline void c_apply(self, double[:] x, double[:] y)
--- src/tensor.pyx
+++ src/tensor.pyx
@@ -25,12 +25,6 @@ def isotropic4(sph, dev, dim):
         The dimension of the physical space on which the returned tensor
         operates.
     """
-    # if dim == 2:
-    #     return IsotropicFourthRankTensor2D(sph, dev)
-    # elif dim == 3:
-    #     return IsotropicFourthRankTensor3D(sph, dev)
-    # else:
-    #     raise ValueError('dim must be 2 or 3 (was {0})'.format(dim))
     return IsotropicFourthRankTensor(sph, dev, dim)
 
 @boundscheck(False)
@@ -81,60 +75,3 @@ cdef class IsotropicFourthRankTensor:
         y = create_or_check_shape_1d(y, self.sym)
         self.c_apply(x, y)
         return y
-
-cdef class IsotropicFourthRankTensor2D:
-
-    @cdivision(True)
-    def __cinit__(self, double sph, double dev):
-        self.dim = 2
-        self.sph = sph
-        self.dev = dev
-        self.s = (sph - dev) / <double> self.dim
-
-    @boundscheck(False)
-    @wraparound(False)
-    cdef inline void c_apply(self, double[:] x, double[:] y):
-        cdef double aux1 = self.s * (x[0] + x[1])
-        cdef double aux2 = self.dev
-        y[0] = aux1 + aux2 * x[0]
-        y[1] = aux1 + aux2 * x[1]
-        y[2] = aux2 * x[2]
-
-    def apply(self, double[:] x, double[:] y=None):
-        check_shape_1d(x, 3)
-        y = create_or_check_shape_1d(y, 3)
-        self.c_apply(x, y)
-        return y
-
-    def __repr__(self):
-        return ('IsotropicFourthRankTensor(sph={0}, dev={1}, dim={2})'.format(self.sph, self.dev, self.dim))
-
-cdef class IsotropicFourthRankTensor3D:
-
-    @cdivision(True)
-    def __cinit__(self, double sph, double dev):
-        self.dim = 3
-        self.sph = sph
-        self.dev = dev
-        self.s = (sph - dev) / <double> self.dim
-
-    @boundscheck(False)
-    @wraparound(False)
-    cdef inline void c_apply(self, double[:] x, double[:] y):
-        cdef double aux1 = self.s * (x[0] + x[1] + x[2])
-        cdef double aux2 = self.dev
-        y[0] = aux1 + aux2 * x[0]
-        y[1] = aux1 + aux2 * x[1]
-        y[2] = aux1 + aux2 * x[2]
-        y[3] = aux2 * x[3]
-        y[4] = aux2 * x[4]
-        y[5] = aux2 * x[5]
-
-    def apply(self, double[:] x, double[:] y=None):
-        check_shape_1d(x, 6)
-        y = create_or_check_shape_1d(y, 6)
-        self.c_apply(x, y)
-        return y
-
-    def __repr__(self):
-        return ('IsotropicFourthRankTensor(sph={0}, dev={1}, dim={2})'.format(self.sph, self.dev, self.dim))
