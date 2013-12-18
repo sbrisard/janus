83fb3af037d9a788e287d77e5e402029c42b5ab5
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2013-12-18 14:20:09 +0100

    In module tensor, restored the classes   - _FourthRankIsotropicTensor2D   - _FourthRankIsotropicTensor3D The constructor of FourthRankIsotropicTensor should never be called directly.

--- src/tensor.pxd
+++ src/tensor.pxd
@@ -1,11 +1,6 @@
-# The signature of the static function allowing application of a fourth rank,
-# isotropic tensor to a second rank, symmetric tensor.
-ctypedef void (*isotropic4_apply_t)(double, double, double[:], double[:])
-
 cdef class FourthRankIsotropicTensor:
     cdef int dim, sym
     cdef readonly double sph, dev
     cdef double tr
-    cdef isotropic4_apply_t static_c_apply
+    cdef void c_apply(self, double[:] x, double[:] y)
 
-    cdef inline void c_apply(self, double[:] x, double[:] y)
--- src/tensor.pyx
+++ src/tensor.pyx
@@ -25,53 +25,58 @@ def isotropic4(sph, dev, dim):
         The dimension of the physical space on which the returned tensor
         operates.
     """
-    return FourthRankIsotropicTensor(sph, dev, dim)
-
-@boundscheck(False)
-@wraparound(False)
-cdef inline void c_apply_2D(double tr_coeff, double dev,
-                            double[:] x, double[:] y):
-    cdef double aux = tr_coeff * (x[0] + x[1])
-    y[0] = aux + dev * x[0]
-    y[1] = aux + dev * x[1]
-    y[2] = dev * x[2]
-
-@boundscheck(False)
-@wraparound(False)
-cdef inline void c_apply_3D(double tr_coeff, double dev,
-                            double[:] x, double[:] y):
-    cdef double aux = tr_coeff * (x[0] + x[1] + x[2])
-    y[0] = aux + dev * x[0]
-    y[1] = aux + dev * x[1]
-    y[2] = aux + dev * x[2]
-    y[3] = dev * x[3]
-    y[4] = dev * x[4]
-    y[5] = dev * x[5]
+    if dim == 2:
+        return _FourthRankIsotropicTensor2D(sph, dev, dim)
+    elif dim == 3:
+        return _FourthRankIsotropicTensor3D(sph, dev, dim)
+    else:
+        raise ValueError('dim must be 2 or 3 (was {0})'.format(dim))
 
 cdef class FourthRankIsotropicTensor:
     @cdivision(True)
     def __cinit__(self, double sph, double dev, int dim):
+        """Should not be called directly, as the returned object would not
+        properly initialized.
+
+        """
         self.dim = dim
         self.sym = (self.dim * (self.dim + 1)) / 2
         self.sph = sph
         self.dev = dev
         self.tr = (sph - dev) / <double> self.dim
-        if dim == 2:
-            self.static_c_apply = c_apply_2D
-        elif dim == 3:
-            self.static_c_apply = c_apply_3D
-        else:
-            raise ValueError('dim must be 2 or 3 (was {0})'.format(dim))
 
     def __repr__(self):
         return ('FourthRankIsotropicTensor(sph={0}, dev={1}, dim={2})'
                 .format(self.sph, self.dev, self.dim))
 
-    cdef inline void c_apply(self, double[:] x, double[:] y):
-        self.static_c_apply(self.tr, self.dev, x, y)
+    cdef void c_apply(self, double[:] x, double[:] y):
+        pass
 
     def apply(self, double[:] x, double[:] y=None):
         check_shape_1d(x, self.sym)
         y = create_or_check_shape_1d(y, self.sym)
         self.c_apply(x, y)
         return y
+
+cdef class _FourthRankIsotropicTensor2D(FourthRankIsotropicTensor):
+    @boundscheck(False)
+    @wraparound(False)
+    cdef inline void c_apply(self, double[:] x, double[:] y):
+        cdef double aux = self.tr * (x[0] + x[1])
+        cdef double self_dev = self.dev
+        y[0] = aux + self_dev * x[0]
+        y[1] = aux + self_dev * x[1]
+        y[2] = self_dev * x[2]
+
+cdef class _FourthRankIsotropicTensor3D(FourthRankIsotropicTensor):
+    @boundscheck(False)
+    @wraparound(False)
+    cdef inline void c_apply(self, double[:] x, double[:] y):
+        cdef double aux = self.tr * (x[0] + x[1] + x[2])
+        cdef double self_dev = self.dev
+        y[0] = aux + self_dev * x[0]
+        y[1] = aux + self_dev * x[1]
+        y[2] = aux + self_dev * x[2]
+        y[3] = self_dev * x[3]
+        y[4] = self_dev * x[4]
+        y[5] = self_dev * x[5]
