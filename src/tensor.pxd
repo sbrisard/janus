68f904b51a56523ccc70765bddd2e1d2cf096e44
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2013-12-18 11:50:06 +0100

    In module tensor, simplification of the hierarchy classes. Through the use of pointers to (static) functions, the unique class   - IsotropicFourthRankTensor replaces the two classes   - IsotropicFourthRankTensor2D,   - IsotropicFourthRankTensor3D.

--- src/tensor.pxd
+++ src/tensor.pxd
@@ -1,3 +1,15 @@
+# The signature of the static function allowing application of a fourth rank,
+# isotropic tensor to a second rank, symmetric tensor.
+ctypedef void (*isotropic4_apply_t)(double, double, double[:], double[:])
+
+cdef class IsotropicFourthRankTensor:
+    cdef int dim, sym
+    cdef readonly double sph, dev
+    cdef double s
+    cdef isotropic4_apply_t static_c_apply
+
+    cdef inline void c_apply(self, double[:] x, double[:] y)
+
 cdef class IsotropicFourthRankTensor2D:
     cdef int dim
     cdef readonly double sph, dev
--- src/tensor.pyx
+++ src/tensor.pyx
@@ -25,12 +25,62 @@ def isotropic4(sph, dev, dim):
         The dimension of the physical space on which the returned tensor
         operates.
     """
-    if dim == 2:
-        return IsotropicFourthRankTensor2D(sph, dev)
-    elif dim == 3:
-        return IsotropicFourthRankTensor3D(sph, dev)
-    else:
-        raise ValueError('dim must be 2 or 3 (was {0})'.format(dim))
+    # if dim == 2:
+    #     return IsotropicFourthRankTensor2D(sph, dev)
+    # elif dim == 3:
+    #     return IsotropicFourthRankTensor3D(sph, dev)
+    # else:
+    #     raise ValueError('dim must be 2 or 3 (was {0})'.format(dim))
+    return IsotropicFourthRankTensor(sph, dev, dim)
+
+@boundscheck(False)
+@wraparound(False)
+cdef inline void c_apply_2D(double tr_coeff, double dev,
+                            double[:] x, double[:] y):
+    cdef double aux = tr_coeff * (x[0] + x[1])
+    y[0] = aux + dev * x[0]
+    y[1] = aux + dev * x[1]
+    y[2] = dev * x[2]
+
+@boundscheck(False)
+@wraparound(False)
+cdef inline void c_apply_3D(double tr_coeff, double dev,
+                            double[:] x, double[:] y):
+    cdef double aux = tr_coeff * (x[0] + x[1] + x[2])
+    y[0] = aux + dev * x[0]
+    y[1] = aux + dev * x[1]
+    y[2] = aux + dev * x[2]
+    y[3] = dev * x[3]
+    y[4] = dev * x[4]
+    y[5] = dev * x[5]
+
+cdef class IsotropicFourthRankTensor:
+    @cdivision(True)
+    def __cinit__(self, double sph, double dev, int dim):
+        self.dim = dim
+        self.sym = (self.dim * (self.dim + 1)) / 2
+        self.sph = sph
+        self.dev = dev
+        self.s = (sph - dev) / <double> self.dim
+        if dim == 2:
+            self.static_c_apply = c_apply_2D
+        elif dim == 3:
+            self.static_c_apply = c_apply_3D
+        else:
+            raise ValueError('dim must be 2 or 3 (was {0})'.format(dim))
+
+    def __repr__(self):
+        return ('IsotropicFourthRankTensor(sph={0}, dev={1}, dim={2})'
+                .format(self.sph, self.dev, self.dim))
+
+    cdef inline void c_apply(self, double[:] x, double[:] y):
+        self.static_c_apply(self.s, self.dev, x, y)
+
+    def apply(self, double[:] x, double[:] y=None):
+        check_shape_1d(x, self.sym)
+        y = create_or_check_shape_1d(y, self.sym)
+        self.c_apply(x, y)
+        return y
 
 cdef class IsotropicFourthRankTensor2D:
 
