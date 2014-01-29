6ac6da1d56f595abcd933f569c192f22296913f0
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-01-29 09:59:08 +0100

    In class janus.local_operator.LocalOperator2D, renamed attribute shape -> global_shape.

--- janus/local_operator.pyx
+++ janus/local_operator.pyx
@@ -7,18 +7,17 @@ from janus.utils.checkarray cimport create_or_check_shape_3d
 from janus.utils.interfaces cimport Operator
 
 cdef class LocalOperator2D:
-    cdef readonly tuple shape
+    cdef readonly tuple global_shape
     cdef readonly int dim
     cdef Operator[:, :] op
     cdef Py_ssize_t n0, n1
     cdef int nrows, ncols
 
     def __cinit__(self, Operator[:, :] op):
-        # TODO correct this line
-        self.shape = None
         self.dim = 2
         self.n0 = op.shape[0]
         self.n1 = op.shape[1]
+        self.global_shape = (self.n0, self.n1)
         self.op = op.copy()
         cdef int i0, i1, nrows, ncols
         for i0 in range(self.n0):
