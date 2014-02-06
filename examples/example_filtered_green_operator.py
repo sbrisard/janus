50f8d37ce32720f293516921ef066063d150fbd6
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-02-06 17:15:44 +0100

    Examples of use of the filtered Green operator.

---
+++ examples/example_filtered_green_operator.py
@@ -0,0 +1,39 @@
+import sys
+
+sys.path.append('..')
+
+import numpy as np
+import skimage.io
+
+skimage.io.use_plugin('freeimage', 'imread')
+skimage.io.use_plugin('freeimage', 'imsave')
+
+import janus.discretegreenop as discretegreenop
+import janus.fft.serial as fft
+import janus.greenop as greenop
+
+from janus.matprop import IsotropicLinearElasticMaterial as Material
+
+mat = Material(1.0, 0.3, 2)
+n0 = 256
+n1 = 256
+n = (n0, n1)
+
+greenc = greenop.create(mat)
+greend = discretegreenop.FilteredGreenOperator2D(greenc, n, 1.)
+
+g = np.empty((n0, n1), dtype=np.float64)
+b = np.empty((2,), dtype=np.intc)
+
+tau = np.array([0., 0., 1.])
+eta = np.empty_like(tau)
+
+for b0 in range(n0):
+    b[0] = b0
+    for b1 in range(n1):
+        b[1] = b1
+        greend.set_frequency(b)
+        greend.apply_by_freq(tau, eta)
+        g[b0, b1] = eta[0]
+
+skimage.io.imsave('bla.tif', g.astype(np.float32))
