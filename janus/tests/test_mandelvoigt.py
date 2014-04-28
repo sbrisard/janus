1e96126c10d52566be34b019a27ba2a9e9edb6a8
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-04-28 10:27:12 +0200

    In file `test_mandelvoigt.py, implemented test of the method create_array.

--- janus/tests/test_mandelvoigt.py
+++ janus/tests/test_mandelvoigt.py
@@ -1,16 +1,23 @@
+import itertools
+
+import numpy as np
 import pytest
+
+from numpy.testing import assert_array_equal
 
 from janus.mandelvoigt import MandelVoigt
 
 
-# def test_create_array_3d():
-#     # TODO finish test
-#     mv = mandelvoigt.get_instance(3)
-#     coeff = lambda i, j, k, l: 1.
-#     a = mv.create_array(coeff)
-#     print(a)
-
 class TestMandelVoigt:
+
+    def pytest_generate_tests(self, metafunc):
+        if metafunc.function.__name__ == 'test_create_array':
+            params = []
+            for dim in [2, 3]:
+                r = range(dim)
+                multi_indices = itertools.product(r, r, r, r)
+                params += [(i, j, k, l, dim) for i, j, k, l in multi_indices]
+            metafunc.parametrize('m, n, p, q, dim', params)
 
     @pytest.mark.parametrize('dim', [2, 3])
     def test_singleton(self, dim):
@@ -56,3 +63,30 @@ class TestMandelVoigt:
                               (3, (1, 0), 5)])
     def test_ravel_multi_index(self, dim, multi_index, expected):
         assert MandelVoigt(dim).ravel_multi_index(*multi_index) == expected
+
+    def test_create_array(self, m, n, p, q, dim):
+        mv = MandelVoigt(dim)
+        mn = mv.ravel_multi_index(m, n)
+        pq = mv.ravel_multi_index(p, q)
+        sym = (dim * (dim + 1)) // 2
+        expected = np.zeros((sym, sym), dtype=np.float64)
+        if mn < dim:
+            if pq < dim:
+                expected[mn, pq] = 1.0
+            else:
+                expected[mn, pq] = np.sqrt(2.0)
+        else:
+            if pq < dim:
+                expected[mn, pq] = np.sqrt(2.0)
+            else:
+                expected[mn, pq] = 2.0
+
+        def coeff(i, j, k, l):
+            ijkl = (i, j, k, l)
+            if (ijkl == (m, n, p, q) or ijkl == (m, n, q, p) or
+                ijkl == (n, m, p, q) or ijkl == (n, m, q, p)):
+                return 1.0
+            else:
+                return 0.0
+        actual = MandelVoigt(dim).create_array(coeff)
+        assert_array_equal(expected, actual)
