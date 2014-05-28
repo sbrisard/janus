044e1002503d517100005b89404acedbe6622d9a
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-05-28 14:49:37 +0200

    Migrated test_memory_leak.py to pytest.

--- janus/tests/test_memory_leak.py
+++ janus/tests/test_memory_leak.py
@@ -1,9 +1,11 @@
+import gc
+
+import pytest
+
 import janus.greenop as greenop
 
 from janus.discretegreenop import truncated
 from janus.matprop import IsotropicLinearElasticMaterial as Material
-
-import gc
 
 def truncated_factory():
     return truncated(greenop.create(Material(1.0, 0.3, 2)),
@@ -15,11 +17,9 @@ def num_memoryviews():
     return sum(1 if 'memoryview' in str(type(o)) else 0
                for o in gc.get_objects())
 
-def test(factory, num_iters):
+@pytest.mark.parametrize('factory, num_iters', [(truncated_factory, 10)])
+def test_memory_leak(factory, num_iters):
+    '''Shows that Issue #5 is solved.'''
     for i in range(num_iters):
         factory()
-        print('Iteration {0},  {1} memoryviews.'.format(i + 1,
-                                                        num_memoryviews()))
-
-if __name__ == '__main__':
-    test(truncated_factory, 10)
+    assert num_memoryviews() == 0
