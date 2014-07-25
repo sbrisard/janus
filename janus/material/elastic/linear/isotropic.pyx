2013daeabd73346da563afd2a4f01c574026763f
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-07-25 07:46:59 +0200

    In module janus.material.elastic.linear.isotropic, allow for the creation of materials with null stiffness (pores).

--- janus/material/elastic/linear/isotropic.pyx
+++ janus/material/elastic/linear/isotropic.pyx
@@ -27,8 +27,8 @@ cdef class IsotropicLinearElasticMaterial:
 
     @cdivision(True)
     def __cinit__(self, double g, double nu, int dim=3):
-        if g <= 0.0:
-            raise ValueError('g must be > 0 (was {0})'.format(g))
+        if g < 0.0:
+            raise ValueError('g must be >= 0 (was {0})'.format(g))
         if nu <= -1.0 or nu >= 0.5:
             raise ValueError('nu must be > -1 and < 1/2 (was {0})'.format(nu))
         if dim != 2 and dim != 3:
