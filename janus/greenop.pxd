f725a9dfe567dcdcf02d3a9f26aac5e9b6b8ea91
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-09-22 13:02:20 +0200

    Docstrings.

--- janus/greenop.pxd
+++ janus/greenop.pxd
@@ -2,23 +2,6 @@ from cython.view cimport array
 from janus.operators cimport AbstractLinearOperator
 
 cdef class AbstractGreenOperator(AbstractLinearOperator):
-
-    """This class defines the periodic Green operator for linear elasticity.
-
-    Parameters
-    ----------
-    mat : IsotropicLinearElasticMaterial
-        Reference material.
-
-    Attributes
-    ----------
-    dim : int
-        Dimension of the physical space.
-    mat : IsotropicLinearElasticMaterial
-        Reference material.
-
-    """
-
     cdef readonly int dim
 
     cdef void c_set_frequency(self, double[:] k)
--- janus/greenop.pyx
+++ janus/greenop.pyx
@@ -10,20 +10,30 @@ from janus.utils.checkarray cimport check_shape_1d
 
 cdef class AbstractGreenOperator(AbstractLinearOperator):
 
+    """This class defines Abstract periodic Green operators.
+
+    Green operators are defined as linear operators, which are
+    supplemented with a `set_frequency()` method.
+
+    The __repr__() method assumes that the attribute mat stores the
+    underlying material.
+
+    Attributes:
+        dim: the dimension of the physical space
+
+    """
+
     cdef void c_set_frequency(self, double[:] k):
         """Set the current wave-vector of this Green operator.
 
-        Any subsequent call to e.g. :func:`c_apply`,
-        :func:`c_to_memoryview` are performed with the specified value
-        of `k`.
+        Any subsequent call to e.g. c_apply(), c_to_memoryview() are
+        performed with the specified value of k.
 
         Concrete implementations of this method are not required to
-        perform any test on the validity (size) of `k`.
+        perform any test on the validity (size) of k.
 
-        Parameters
-        ----------
-        k : memoryview of float64
-            The current wave-vector.
+        Args:
+            k: the wave-vector (memoryview of float64)
 
         """
         raise NotImplementedError
@@ -31,13 +41,14 @@ cdef class AbstractGreenOperator(AbstractLinearOperator):
     def set_frequency(self, double[:] k):
         """Set the current wave-vector of this Green operator.
 
-        Any subsequent call to e.g. :func:`apply`, :func:`to_memoryview`
-        are performed with the specified value of `k`.
+        Any subsequent call to e.g. apply(), to_memoryview() are
+        performed with the specified value of k.
 
-        Parameters
-        ----------
-        k : memoryview of float64
-            The current wave-vector.
+        Concrete implementations of this method are not required to
+        perform any test on the validity (size) of k.
+
+        Args:
+            k: the wave-vector (memoryview of float64)
 
         """
         check_shape_1d(k, self.dim)
