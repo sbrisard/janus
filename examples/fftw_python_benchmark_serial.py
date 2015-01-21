4014410a89e07bf07843348ab3f01ade3d9fefdb
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2015-01-21 07:58:04 +0100

    Benchmark of the serial FFTW wrapper.

---
+++ examples/fftw_python_benchmark_serial.py
@@ -0,0 +1,32 @@
+import sys
+import time
+
+import numpy as np
+
+import janus.fft.serial
+
+def benchmark(shape, niter):
+    transform = janus.fft.serial.create_real(shape)
+    r = np.random.uniform(-1., 1., transform.shape)
+    c = np.empty(transform.cshape, dtype=np.float64)
+    times = []
+    for i in range(niter):
+        t1 = time.perf_counter()
+        transform.r2c(r, c)
+        t2 = time.perf_counter()
+        times.append(1E3 * (t2 - t1))
+
+    return np.mean(times), np.std(times)
+
+if __name__ == '__main__':
+
+    nprnd.seed(20140121)
+
+    params = [((128, 128, 128), 15000),
+              ((256, 256, 256), 10000),
+              ((512, 512, 512), 1000)]
+
+    for shape, niter in params:
+        mean, std = benchmark(shape, niter)
+        args = map(str, shape + (niter, 1, mean, std))
+        print(','.join(args), flush=True)
