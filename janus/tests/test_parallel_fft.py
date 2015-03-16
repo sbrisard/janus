3f2ed7575138b7b3ea2b041c22c047544fd5b0ca
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2015-03-16 10:27:34 +0100

    Corrected calls to comm.Gatherv()/comm.Scatterv().

--- janus/tests/test_parallel_fft.py
+++ janus/tests/test_parallel_fft.py
@@ -9,23 +9,29 @@ from mpi4py import MPI
 
 # TODO Scatterv fails with arrays of unequal size
 # (which happens with n0 == 31)
-@pytest.mark.parametrize('shape', [#(31, 15),
-                                   #(31, 16),
-                                   (32, 15),
-                                   (32, 16)])
+@pytest.mark.parametrize('shape', [(31, 15), (31, 16), (32, 15), (32, 16)])
 def test_r2c(shape):
     comm = MPI.COMM_WORLD
     root = 0
 
     janus.fft.parallel.init()
     pfft = janus.fft.parallel.create_real(shape, comm)
+    # TODO Define isize and osize for FFT objects.
+    sendobj = (np.product(pfft.rshape),
+               pfft.offset0 * np.product(pfft.rshape[1:]),
+               np.product(pfft.cshape),
+               pfft.offset0 * np.product(pfft.cshape[1:]))
+    counts_and_displs = comm.gather(sendobj=sendobj, root=root)
+
     if comm.rank == root:
         np.random.seed(20150312)
         rglob = 2. * np.random.rand(*shape) - 1.
+        icounts, idispls, ocounts, odispls = zip(*counts_and_displs)
     else:
         rglob = None
+        icounts, idispls, ocounts, odispls = None, None, None, None
     rloc = np.empty(pfft.rshape, dtype=np.float64)
-    comm.Scatterv(rglob, rloc, root)
+    comm.Scatterv([rglob, icounts, idispls, MPI.DOUBLE], rloc, root)
     cloc = np.empty(pfft.cshape, dtype=np.float64)
     pfft.r2c(rloc, cloc)
     if comm.rank == root:
@@ -34,7 +40,7 @@ def test_r2c(shape):
                           dtype=np.float64)
     else:
         actual = None
-    comm.Gatherv(cloc, actual, root)
+    comm.Gatherv(cloc, [actual, ocounts, odispls, MPI.DOUBLE], root)
 
     if comm.rank == root:
         sfft = janus.fft.serial.create_real(shape)
