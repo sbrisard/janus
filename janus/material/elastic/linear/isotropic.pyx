3290be1f8f19a77fc2c85dd74708acb6d086d6fb
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2015-03-19 08:35:20 +0100

    Improved preconditions for isotropic, linear, elastic materials.

--- janus/material/elastic/linear/isotropic.pyx
+++ janus/material/elastic/linear/isotropic.pyx
@@ -81,8 +81,10 @@ cdef class IsotropicLinearElasticMaterial:
     def __cinit__(self, double g, double nu, int dim=3):
         if g < 0.0:
             raise ValueError('g must be >= 0 (was {0})'.format(g))
-        if nu <= -1.0 or nu >= 0.5:
-            raise ValueError('nu must be > -1 and < 1/2 (was {0})'.format(nu))
+        if dim == 2 and nu > 0.5:
+            raise ValueError('nu must be <= 1/2 (was {0})'.format(nu))
+        if dim == 3 and (nu < -1.0 or nu > 0.5):
+            raise ValueError('nu must be >= -1 and <= 1/2 (was {0})'.format(nu))
         if dim != 2 and dim != 3:
             raise ValueError('dim must be 2 or 3 (was {0})'.format(dim))
         self.g = g
