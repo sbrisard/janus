f0f32eed4a23c8617d12e7f752ed2f381aa2f3f0
Sebastien Brisard <sebastien.brisard@m4x.org>
2018-07-13 08:25:27 +0200

    Rename directory dir docs → sphinx

--- sphinx/make.bat
+++ sphinx/make.bat
@@ -45,10 +45,10 @@ if "%1" == "clean" (
 )
 
 if "%1" == "html" (
-	%SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%/html
+	%SPHINXBUILD% -b html %ALLSPHINXOPTS% ../docs
 	if errorlevel 1 exit /b 1
 	echo.
-	echo.Build finished. The HTML pages are in %BUILDDIR%/html.
+	echo.Build finished. The HTML pages are in ../docs
 	goto end
 )
 
